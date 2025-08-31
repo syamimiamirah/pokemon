6af3a503027f30db27fb4a174514458d33ce0495
syamimiamirah <syamimiamirah@graduate.utm.my>
2025-08-31 18:07:06 +0800

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/pokemon/'   // ⚠️ must match your repo name
+  base: '/'   // ⚠️ must match your repo name
 })
