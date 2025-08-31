6d405448eb09b7b6d3567bdfeae59b5eda90067b
syamimiamirah <syamimiamirah@graduate.utm.my>
2025-08-31 19:53:27 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory("/pokemon/"),
+  history: createWebHistory(process.env.NODE_ENV === 'production' ? '/pokemon/' : '/'),
   routes,
 })
 
