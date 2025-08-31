17085acb5194224a8fa14ffd6d6096e8e52b4c6c
syamimiamirah <syamimiamirah@graduate.utm.my>
2025-08-31 18:17:50 +0800

    deploy

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import Home from "../views/HomeView.vue"
 import PokemonDetail from "../views/PokemonDetail.vue"
 
 const routes = [
-  { path: "/", name: "Home", component: Home },
+  { path: "/pokemon", name: "Home", component: Home },
   { path: "/pokemon/:id", name: "PokemonDetail", component: PokemonDetail, props: true }
 ]
 
