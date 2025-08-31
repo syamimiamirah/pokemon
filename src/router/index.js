326c3506ebfec5ecaaa68952b9914bcedc8cc7d0
syamimiamirah <syamimiamirah@graduate.utm.my>
2025-08-31 18:53:20 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
-import { createRouter, createWebHashHistory } from "vue-router"
-import Home from "../views/HomeView.vue"
-import PokemonDetail from "../views/PokemonDetail.vue"
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/HomeView.vue'
+import PokemonDetail from '../views/PokemonDetail.vue'
 
 const routes = [
-  { path: "/", name: "Home", component: Home },
-  { path: "/:id", name: "PokemonDetail", component: PokemonDetail, props: true }
+  { path: '/pokemon', name: 'Home', component: Home },
+  { path: '/pokemon/:id', name: 'PokemonDetail', component: PokemonDetail, props: true },
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 })
 
